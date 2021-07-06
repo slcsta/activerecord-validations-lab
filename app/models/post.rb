b82c7c73439ad768161fadd5c1a196012ff5f347
Sarah <sarahlchavez@gmail.com>
2021-07-06 07:16:14 -0700

    commit tree 6abeaed98cfa63b499b16b29d811b681f2ca5885

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid choice" }
+    validate :title_invalid_if_not_clickbait
+
+
+    def title_invalid_if_not_clickbait
+        if title && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Title not clickbait-y")
+        end
+    end
 end
